d7665ddc55e42ff75694ddf6b7e35a601caa2a9d
YasminMattos <yasmin.mattos.conislla@gmail.com>
2017-11-19 01:07:49 -0500

    version 0.0.2

--- js/app.js
+++ js/app.js
@@ -1,16 +1,32 @@
 window.addEventListener('load', function(){
+  var section = document.querySelector('section');
   var tab = document.getElementById('tab');
   var item = document.querySelector('.item');
-  var add = document.createElement('button');
-  add.textContent = 'Save';
+  var save = document.createElement('button');
+  save.textContent = 'Save';
 
   item.addEventListener('click', function(){
     item.classList.add('item-js');
     tab.classList.add('tab-js');
-    add.classList.add('add-js');
-    tab.appendChild(add);
-    
-  })
+    save.classList.add('save-js');
+    tab.appendChild(save);
+  });
+
+  save.addEventListener('click', function(){
+    var container = document.createElement('div');
+    container.classList.add('container-js');
+    section.appendChild(container);
+    var listName = document.createElement('h4');
+    listName.textContent = item.value;
+    listName.classList.add('list-name-js');
+    container.appendChild(listName);
+    var card = document.createElement('input');
+    card.setAttribute('placeholder', 'Add a card...');
+    card.classList.add('card-js');
+    container.appendChild(card);
+  });
+
+
 
 })
 
