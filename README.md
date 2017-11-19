a170a1b5c2e6339884fddae5c622656b87a1279d
YasminMattos <yasmin.mattos.conislla@gmail.com>
2017-11-19 15:58:01 -0500

    añade readme

---
+++ README.md
@@ -0,0 +1,29 @@
+# **Reto Trello**
+-----
+
+### **Objetivo**
+
+Replicar el tablero de Trello.
+
+### **Contenido**
+
+La réplica contiene ciertas funcionalidades especificadas en cada versión:
+
+***Versión 0.0.1:***  
+
+- Muestra el texto "Añadir una lista ...".
+
+- Al hacer click se oculta el texto y muestra un formulario.
+
+- El formulario está conformado por un input y un botón para que pueda añadir tareas a tu lista.
+
+***Versión 0.0.2***
+
+- Al dar click en el botón de "Guardar", se muestra un nuevo cuadro donde aparece el nombre de la lista agregada.
+
+- Muestra un texto de "Añadir una tarea" dentro de la lista.
+
+
+***_Resultado:_*** 
+
+![trello](assets/images/trellotab.png)
--- js/app.js
+++ js/app.js
@@ -3,27 +3,41 @@ window.addEventListener('load', function(){
   var tab = document.getElementById('tab');
   var item = document.querySelector('.item');
   var save = document.createElement('button');
-  save.textContent = 'Save';
+  var close = document.createElement('button');
 
   item.addEventListener('click', function(){
+    save.textContent = 'Save';
     item.classList.add('item-js');
     tab.classList.add('tab-js');
     save.classList.add('save-js');
     tab.appendChild(save);
+    close.classList.add('btn-js');
+    close.classList.add('icon-cross');
+    tab.appendChild(close);
+  });
+
+  close.addEventListener('click', function(){
+    tab.removeChild(save);
+    item.classList.remove('item-js');
+    tab.classList.remove('tab-js');
+    tab.removeChild(close);
+        
   });
 
   save.addEventListener('click', function(){
-    var container = document.createElement('div');
-    container.classList.add('container-js');
-    section.appendChild(container);
-    var listName = document.createElement('h4');
-    listName.textContent = item.value;
-    listName.classList.add('list-name-js');
-    container.appendChild(listName);
-    var card = document.createElement('input');
-    card.setAttribute('placeholder', 'Add a card...');
-    card.classList.add('card-js');
-    container.appendChild(card);
+    if (item.value.length !== 0)
+      var container = document.createElement('div');
+      container.classList.add('container-js');
+      section.appendChild(container);
+      var listName = document.createElement('h4');
+      listName.textContent = item.value;
+      listName.classList.add('list-name-js');
+      container.appendChild(listName);
+      item.value = '';
+      var card = document.createElement('input');
+      card.setAttribute('placeholder', 'Add a card...');
+      card.classList.add('card-js');
+      container.appendChild(card);
   });
 
 
